db23d2dba365b5c283a03f640c601ffe518051d9
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-02-26 14:16:36 -0300

    add: link component to brand logo

--- src/components/Navbar/mySeriesLogo/index.jsx
+++ src/components/Navbar/mySeriesLogo/index.jsx
@@ -1,11 +1,16 @@
+import Link from 'next/link'
 
 export default function mySeriesLogo() {
   return (
-    <div className='navbar-brand text-info'>
-      <i className='bi bi-tv me-md-2' />
-      <span className='d-none d-md-inline-block'>
-        mySeries
-      </span>
-    </div>
+    <Link href='/'>
+      <a>
+        <div className='navbar-brand text-info'>
+          <i className='bi bi-tv me-md-2' />
+          <span className='d-none d-md-inline-block'>
+            mySeries
+          </span>
+        </div>
+      </a>
+    </Link>
   )
 }
