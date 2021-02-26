057ac33cc6b2fbf66f0cce0ed774324f2df80537
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-02-26 14:26:05 -0300

    add: initial content wrapper

---
+++ src/components/ContentWrapper/index.jsx
@@ -0,0 +1,8 @@
+
+export default function ContentWrapper({ children }) {
+  return (
+    <main className='container-fluid border rounded-top mt-2'>
+      { children }
+    </main>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
+import ContentWrapper from '../components/ContentWrapper'
 
 export default function Home() {
   return (
     <Layout>
       <Navbar />
+      <ContentWrapper>
+        <h1>mySeries</h1>
+      </ContentWrapper>
     </Layout>
   )
 }
