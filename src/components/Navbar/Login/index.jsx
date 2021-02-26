ae2899005022ecd23ff497f2210fbfd437beefd0
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-02-26 14:16:59 -0300

    add: initial login button

--- src/components/Navbar/Login/index.jsx
+++ src/components/Navbar/Login/index.jsx
@@ -1,39 +1,10 @@
 
 export default function Login() {
   return (
-    <>
-      <button className='btn btn-outline-info' type='button' data-toggle='modal' data-target='#form-login'>Login</button>
-      {/* <!-- Modal Cadastro --> */}
-      <div className='modal fade' id='form-login' tabIndex='-1'>
-        <div className='modal-dialog'>
-          <div className='modal-content'>
-            <div className='modal-header'>
-              <h5 className='modal-title'>Login</h5>
-              <button type='button' className='close' data-dismiss='modal'>
-                <span>&times;</span>
-              </button>
-            </div>
-            <form action='' method=''>
-              <div className='modal-body'>
-                <div className='form-row'>
-                  <div className='col-5'>
-                    <input type='text' className='form-control' name='nickname' placeholder='Escolha seu nickname...' />
-                  </div>
-                  <div className='col-5'>
-                    <input type='password' className='form-control' name='senha' placeholder='Escolha sua senha...' />
-                  </div>
-                  <div className='col-2'>
-                    <button type='submit' className='btn btn-primary w-100'>
-                      <i className='fas fa-paper-plane'></i>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      {/* <!-- / Modal Cadastro --></div> */}
-    </>
+    <div className='ms-2'>
+      <button type='button' className='btn btn-outline-info'>
+        <i className='bi bi-person-circle' />
+      </button>
+    </div>
   )
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,15 @@
 import MySeriesLogo from './mySeriesLogo'
 import SearchForm from './SearchForm'
+import Login from './Login'
 
 export default function NavBar() {
   return (
-    <nav className='container-fluid navbar navbar-dark bg-dark'>
+    <nav className='container-fluid navbar navbar-dark bg-dark rounded-bottom'>
       <MySeriesLogo />
-      <SearchForm />
+      <div className='d-flex justify-content-between'>
+        <SearchForm />
+        <Login />
+      </div>
     </nav>
   )
 }
