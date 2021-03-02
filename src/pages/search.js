2e7bd66cb562f7ddd6ef15675565e15673795b99
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-03-02 19:15:49 -0300

    add: search page and methods

---
+++ src/pages/search.js
@@ -0,0 +1,35 @@
+import tmdbRequest from '../libs/tmdbRequest'
+import handleSearchResults from '../libs/handleArrayMap'
+
+import Layout from '../components/Layout'
+import ResultsWrapper from '../components/CardsWrapper'
+
+export default function Home({ results }) {
+
+  return (
+    <Layout>
+      <ResultsWrapper title={'Resultados'}>
+        {handleSearchResults(results, 'Nenhum resultado encontrado')}
+      </ResultsWrapper>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const { type, value } = context.query
+  if (!type || !value) {
+    return { notFound: true }
+  }
+
+  const params = {
+    query: value,
+    language: 'pt-BR'
+  }
+  const { results } = await tmdbRequest(`/search/${type}`, params)
+
+  return {
+    props: {
+      results
+    }
+  }
+}
