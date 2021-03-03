143e84df00d1f538a67794ef4f7cc5560c620330
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-03-03 00:35:58 -0300

    add: search input methods for Next/Router use

--- src/components/Navbar/SearchForm/index.jsx
+++ src/components/Navbar/SearchForm/index.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 export default function SearchForm() {
 
+  const router = useRouter()
   let [searchValue, setSearchValue] = useState('')
 
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    router.push(`/search?type=tv&value=${searchValue}`)
+  }
+
   return (
-    <form className='form-inline'>
+    <form
+      className='form-inline'
+      onSubmit={e => handleFormSubmit(e)}
+    >
       <div className='input-group'>
         <input
           id='searchSeries'
@@ -15,9 +25,12 @@ export default function SearchForm() {
           value={searchValue}
           onChange={e => setSearchValue(e.target.value)}
         />
-        <div className='input-group-text btn bg-info border-info'>
+        <button
+          type='submit'
+          className='input-group-text btn bg-info border-info'
+        >
           <i className='bi bi-search' />
-        </div>
+        </button>
       </div>
     </form>
   )
