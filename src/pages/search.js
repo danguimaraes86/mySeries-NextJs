26897b57835d1099afcfc54e225837cd482b6822
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-03-03 00:18:50 -0300

    refact: move Navbar for better reuse

--- src/pages/search.js
+++ src/pages/search.js
@@ -2,12 +2,14 @@ import tmdbRequest from '../libs/tmdbRequest'
 import handleSearchResults from '../libs/handleArrayMap'
 
 import Layout from '../components/Layout'
+import NavBar from '../components/Navbar'
 import ResultsWrapper from '../components/CardsWrapper'
 
 export default function Home({ results }) {
 
   return (
     <Layout>
+      <NavBar />
       <ResultsWrapper title={'Resultados'}>
         {handleSearchResults(results, 'Nenhum resultado encontrado')}
       </ResultsWrapper>
