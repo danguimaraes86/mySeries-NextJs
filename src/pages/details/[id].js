9b38a0ab5bf01132d2adb8680111dff183ed1266
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-03-03 12:24:24 -0300

    add: tmdb api request for series details

--- src/pages/details/[id].js
+++ src/pages/details/[id].js
@@ -1,6 +1,10 @@
+import tmdbRequest from '../../libs/tmdbRequest'
+
 import Layout from '../../components/Layout'
 
 export default function SeriesDetails({ series }) {
+  console.log(series)
+  
   return (
     <Layout>
       <h1>Series Details: {series.id}</h1>
@@ -10,15 +14,14 @@ export default function SeriesDetails({ series }) {
 
 export async function getServerSideProps(context) {
   const { id } = context.query
-
-  const series = {
-    id: id
+  const params = {
+    language: 'pt-BR',
+    append_to_response: 'external_ids'
   }
-  // const params = {
-  //   query: value,
-  //   language: 'pt-BR'
-  // }
-  // const { results } = await tmdbRequest('/search/tv', params)
+  const series = await tmdbRequest(`/tv/${id}`, params)
+  const { results } = await tmdbRequest(`/tv/${id}/watch/providers`)
+
+  series['providers'] = results.BR.flatrate
 
   return {
     props: {
