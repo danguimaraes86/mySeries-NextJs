9d278fbc5e19d602f6e1a6859b845987c0e127e7
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-03-05 00:18:17 -0300

    add: next/image component and null poster validation

--- src/components/SeriesCard/index.jsx
+++ src/components/SeriesCard/index.jsx
@@ -1,14 +1,36 @@
 import { getYear } from '../../libs/dateParsing'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function SeriesCard({ poster, name, year, id }) {
+
+  function handleSeriesPoster(img){
+    if(!img){
+      return (
+        <div className='card-img-top d-flex justify-content-center align-items-center bg-secondary h-100'>
+          <i className='bi bi-image text-info ' style={{ fontSize: '6em' }} />
+        </div>
+      )
+    } else {
+      return (
+        <Image 
+          src={series_poster_url}
+          width={500}
+          height={800}
+          className='card-img-top'
+          alt={`poster da série ${name}`}
+        />
+      )
+    }
+  }
+
   const series_poster_url = `https://image.tmdb.org/t/p/w500/${poster}`
   const series_year = getYear(year)
   
   return (
     <div className='col'>
       <div className='card h-100'>
-        <img src={series_poster_url} className='card-img-top' alt={`poster da série ${name}`} />
+        {handleSeriesPoster(poster)}
         <div className='card-body'>
           <h5 className='card-title'>{`${name} (${series_year})`}</h5>
         </div>
