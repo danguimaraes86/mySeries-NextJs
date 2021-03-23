2511e3d47bcb20fff84065c2a778e6d9ec0c25cc
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-03-23 12:50:11 -0300

    refact: new details components

--- src/pages/details/[id].js
+++ src/pages/details/[id].js
@@ -1,112 +1,47 @@
 import tmdbRequest from '../../libs/tmdbRequest'
 import { getYear } from '../../libs/dateParsing'
 
-import Image from 'next/image'
 import Layout from '../../components/Layout'
+import DetailsWrapper from '../../components/SeriesDetails/DetailsWrapper'
+import LeftColumn from '../../components/SeriesDetails/DetailsWrapper/LeftColumn'
+import RightColumn from '../../components/SeriesDetails/DetailsWrapper/RightColumn'
+
+import PosterCard from '../../components/SeriesDetails/PosterCard'
+import FavoriteButton from '../../components/SeriesDetails/FavoriteButton'
+import SeriesTitle from '../../components/SeriesDetails/SeriesTitle'
+import SeriesOverview from '../../components/SeriesDetails/SeriesOverview'
+import NetworksAndStreaming from '../../components/SeriesDetails/NetworksAndStreaming'
 
 export default function SeriesDetails({ series }) {
   const series_poster_url = `https://image.tmdb.org/t/p/w500/${series.poster_path}`
   const series_year = getYear(series.first_air_date)
 
-  function handleProvider(series) {
-    if (series.providers.length === 0) {
-      return (
-        <span className='lh-1'>Nenhum encontrado</span>
-      )
-    } else {
-      return series.providers.map((provider, index) => {
-        return (
-          <img
-            key={index}
-            className='img-fluid'
-            style={{ height: '2em' }}
-            src={`https://image.tmdb.org/t/p/w185/${provider.logo_path}`}
-          />
-        )
-      })
-    }
-  }
-
   return (
     <Layout>
-      <main className='border rounded-top p-3 mt-2'>
-        <div className='row'>
+      <DetailsWrapper>
 
-          <div className='col-3'>
+        <LeftColumn>
+          <PosterCard
+            url={series_poster_url}
+            name={series.name}
+          />
+          <FavoriteButton />
+        </LeftColumn>
 
-            <div className='row'>
-              <div className='col-12'>
-                <Image
-                  className='rounded'
-                  src={series_poster_url}
-                  width={500}
-                  height={800}
-                  alt={`poster da série ${series.name}`}
-                />
-              </div>
-
-              <div className='col-12'>
-                <button className='btn btn-outline-dark w-100'>
-                  <span className='d-none d-md-inline-block'>Favorito</span>
-                  <i className='bi bi-bookmark-star' />
-                </button>
-              </div>
-            </div>
-
-          </div>
-
-          <div className='col'>
-            <div className='row'>
-
-              <div className='col-12 mb-4'>
-                <span className='display-5'>
-                  {series.name} {series_year}
-                </span>
-                <br />
-                <span className='text-muted'>
-                  {series.original_name} ({series.original_language.toUpperCase()})
-                </span>
-              </div>
-
-              <div className='col-12 mb-2'>
-                <h6>Descrição</h6>
-                <p>{series.overview}</p>
-              </div>
-
-              <div className='col-12 mb-2'>
-                <div className='row'>
-
-                  <div className='col-6 col-md-4'>
-                    <h6>Networks</h6>
-                    <div className='d-flex justify-content-between flex-wrap'>
-                      <img
-                        className='img-fluid'
-                        style={{ height: '2em' }}
-                        src={`https://image.tmdb.org/t/p/w185/${series.networks[0].logo_path}`}
-                      />
-                    </div>
-                  </div>
-
-                  <div className='col-6 col-md-4'>
-                    <h6>Streaming</h6>
-                    <div className='d-flex justify-content-between flex-wrap'>
-                      {handleProvider(series)}
-                    </div>
-                  </div>
-
-                  {/* <div className='col-4'>
-                    <h6>Popularidade</h6>
-                    <div className='fs-2 lh-1'>
-                      {series.vote_average}
-                    </div>
-                  </div> */}
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+        <RightColumn>
+          <SeriesTitle
+            name={series.name}
+            original_name={series.original_name}
+            original_language={series.original_language}
+            year={series_year}
+          />
+          <SeriesOverview overview={series.overview} />
+          <NetworksAndStreaming
+            network={series.networks[0]}
+            providers={series.providers}
+          />
+        </RightColumn>
+      </DetailsWrapper>
     </Layout >
   )
 }
