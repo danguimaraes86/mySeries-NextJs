770517f062c3d75392f2a299233a524aca32e495
Daniel Guimarães <danguimaraes86@gmail.com>
2021-04-13 18:08:45 -0300

    fix: small design changes

--- src/components/SeriesDetails/NetworksAndStreaming/index.jsx
+++ src/components/SeriesDetails/NetworksAndStreaming/index.jsx
@@ -11,7 +11,7 @@ export default function SeriesTitle({ network, providers }) {
         return (
           <img
             key={index}
-            className='img-fluid'
+            className='img-fluid me-2'
             style={{ height: '2em' }}
             src={`https://image.tmdb.org/t/p/w185/${provider.logo_path}`}
           />
@@ -24,20 +24,20 @@ export default function SeriesTitle({ network, providers }) {
     <div className='col-12 mb-2'>
       <div className='row'>
 
-        <div className='col-6 col-md-4'>
+        <div className='col-6 col-md-4 col-lg-3'>
           <h6>Emissoras</h6>
-          <div className='d-flex justify-content-between flex-wrap'>
+          <div className='d-flex flex-wrap'>
             <img
-              className='img-fluid'
+              className='img-fluid me-2'
               style={{ height: '2em' }}
               src={`https://image.tmdb.org/t/p/w185/${network.logo_path}`}
             />
           </div>
         </div>
 
-        <div className='col-6 col-md-4'>
+        <div className='col-6 col-md-4 col-lg-3'>
           <h6>Onde Assistir</h6>
-          <div className='d-flex justify-content-between flex-wrap'>
+          <div className='d-flex flex-wrap'>
             {handleProvider(providers)}
           </div>
         </div>
