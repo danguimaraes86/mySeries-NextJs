b06d48945f0d74d61f39f66df0cb12f77fa73c06
Daniel Guimarães <danguimaraes86@gmail.com>
2021-05-04 11:10:23 -0300

    add: login and user registration form design

--- src/components/LoginModal/index.jsx
+++ src/components/LoginModal/index.jsx
@@ -13,22 +13,24 @@ export default function LoginModal() {
           <div className='modal-body p-0'>
             <div className='nav nav-tabs nav-fill' role='tablist'>
               <div className='nav-link rounded-0 active' id='nav-login-tab' data-bs-toggle='tab' data-bs-target='#nav-login' type='button' role='tab'>Login</div>
-              <div className='nav-link rounded-0' id='nav-create-tab' data-bs-toggle='tab' data-bs-target='#nav-create' type='button' role='tab'>Cadastro</div>
+              <div className='nav-link rounded-0' id='nav-registration-tab' data-bs-toggle='tab' data-bs-target='#nav-registration' type='button' role='tab'>Cadastro</div>
             </div>
 
             <div className='tab-content'>
+
+              {/* Login Panel */}
               <div className='tab-pane p-3 fade show active' id='nav-login' role='tabpanel'>
                 <form>
-                  <div className='row mb-3'>
-                    <label htmlFor='loginIdInpunt' className='col-3 col-form-label'>Usuário ID</label>
+                  <div className='row g-1 mb-3'>
+                    <label htmlFor='loginIDInpunt' className='col-3 col-form-label'>Usuário ID</label>
                     <div className='col'>
-                      <input type='text' className='form-control' id='loginIdInpunt' />
+                      <input type='text' className='form-control' id='loginIDInpunt' />
                     </div>
                   </div>
-                  <div className='row mb-3'>
-                    <label htmlFor='passwordInput' className='col-3 col-form-label'>Password</label>
+                  <div className='row g-1 mb-3'>
+                    <label htmlFor='loginPwdInput' className='col-3 col-form-label'>Password</label>
                     <div className='col'>
-                      <input type='password' className='form-control' id='passwordInput' />
+                      <input type='password' className='form-control' id='loginPwdInput' />
                     </div>
                   </div>
                   <div className='d-flex justify-content-end'>
@@ -36,31 +38,53 @@ export default function LoginModal() {
                       <span className='me-1'>
                         Login
                       </span>
-                      <i class="bi bi-caret-right" />
+                      <i className="bi bi-caret-right" />
                     </button>
                   </div>
                 </form>
               </div>
-              <div className='tab-pane p-3 fade' id='nav-create' role='tabpanel'>
+
+              {/* Registration Panel */}
+              <div className='tab-pane p-3 fade' id='nav-registration' role='tabpanel'>
                 <form>
-                  <div className='row mb-3'>
-                    <label htmlFor='loginIdInpunt' className='col-3 col-form-label'>Usuário ID</label>
+                  {/* User ID */}
+                  <div className='row g-1 mb-3'>
+                    <label className=' col-3 col-form-label' htmlFor='registrationIDInpunt'>Usuário ID</label>
                     <div className='col'>
-                      <input type='text' className='form-control' id='loginIdInpunt' />
+                      <input type='text' className='form-control' id='registrationIDInpunt' placeholder='Escolha um ID para seu usuário' />
+                    </div>
+                    <div className='col-9 offset-3 form-text'>
+                      Mínimo de 6 caracteres
                     </div>
                   </div>
-                  <div className='row mb-3'>
-                    <label htmlFor='passwordInput' className='col-3 col-form-label'>Password</label>
-                    <div className='col'>
-                      <input type='password' className='form-control' id='passwordInput' />
+                  {/* User Name */}
+                  <div className='row g-1 mb-3'>
+                    <label className=' col-3 col-form-label' htmlFor='registrationNameInpunt'>Nome</label>
+                    <div className='col input-group'>
+                      <input type='text' className='form-control' id='registrationNameInpunt' placeholder='Nome' />
+                      <input type='text' className='form-control' id='registrationLastNameInpunt' placeholder='Sobrenome' />
                     </div>
                   </div>
+                  {/* Password */}
+                  <div className='row g-1 mb-3'>
+                    <label className='col-3 col-form-label' htmlFor='passwordInput' >Password</label>
+                    <div className='col'>
+                      <input type='password' className='form-control' id='passwordInput' placeholder='Digite seu password' />
+                    </div>
+                    <div className='col-9 offset-3'>
+                      <input type='password' className='form-control' id='passwordConfirmationInput' placeholder='Confirme seu password' />
+                    </div>
+                    <div className='col-9 offset-3 form-text'>
+                      Mínimo de 6 caracteres
+                    </div>
+                  </div>
+                  {/* Submit Button */}
                   <div className='d-flex justify-content-end'>
                     <button type='submit' className='btn btn-primary'>
                       <span className='me-2'>
                         Cadastrar
                       </span>
-                      <i class="bi bi-person-plus-fill" />
+                      <i className="bi bi-person-plus-fill" />
                     </button>
                   </div>
                 </form>
