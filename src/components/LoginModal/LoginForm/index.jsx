8453bef9fb2285c04e7062572154c4e7a7d76d19
Daniel Guimarães <danguimaraes86@gmail.com>
2021-05-07 10:54:39 -0300

    add: controlled login form

--- src/components/LoginModal/LoginForm/index.jsx
+++ src/components/LoginModal/LoginForm/index.jsx
@@ -1,20 +1,47 @@
+import { useState } from 'react'
 
 export default function LoginForm() {
+
+  let [userID, setUserID] = useState('')
+  let [password, setPassword] = useState('')
+
+  function handleLoginSubmit(e) {
+    e.preventDefault()
+    console.log({
+      userID,
+      password,
+    })
+  }
+
   return (
     < div className='tab-pane p-3 fade show active' id='nav-login' role='tabpanel' >
-      <form>
+      <form onSubmit={e => handleLoginSubmit(e)}>
         {/* User ID */}
         <div className='row g-1 mb-3'>
           <label htmlFor='loginIDInpunt' className='col-3 col-form-label'>Usuário ID</label>
           <div className='col'>
-            <input type='text' className='form-control' id='loginIDInpunt' />
+            <input
+              type='text'
+              className='form-control'
+              id='loginIDInpunt'
+              placeholder='Digite seu ID de usuário'
+              value={userID}
+              onChange={e => setUserID(e.target.value)}
+            />
           </div>
           {/* Password */}
         </div>
         <div className='row g-1 mb-3'>
           <label htmlFor='loginPwdInput' className='col-3 col-form-label'>Password</label>
           <div className='col'>
-            <input type='password' className='form-control' id='loginPwdInput' />
+            <input
+              type='password'
+              className='form-control'
+              id='loginPwdInput'
+              placeholder='Digite seu password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
           </div>
         </div>
         {/* Submit Button */}
