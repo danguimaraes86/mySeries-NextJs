1acb73779b1d6bc1d5edf773f103afbc983dc754
Daniel Guimarães <danguimaraes86@gmail.com>
2021-05-07 10:23:27 -0300

    refact: added vercel logic

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -2,23 +2,35 @@ import { MongoClient } from 'mongodb'
 
 const { MONGODB_URI, MONGODB_DB } = process.env
 
-let cachedClient = null
-let cachedDb = null
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongo
+
+if (!cached) {
+  cached = global.mongo = { conn: null, promise: null }
+}
 
 export async function connectToDatabase() {
-  if (cachedClient && cachedDb) {
-    return { client: cachedClient, db: cachedDb }
+  if (cached.conn) {
+    return cached.conn
   }
 
-  const client = await MongoClient.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
 
-  const db = await client.db(MONGODB_DB)
-
-  cachedClient = client
-  cachedDb = db
-
-  return { client, db }
+    cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
+      return {
+        client,
+        db: client.db(MONGODB_DB),
+      }
+    })
+  }
+  cached.conn = await cached.promise
+  return cached.conn
 }
