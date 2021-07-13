5663c44ab472cb0edb12138427f54b5da412ea08
Daniel Guimarães <danguimaraes86@gmail.com>
2021-07-13 16:38:44 -0300

    fix: layout fix for Safari

--- src/components/AuthenticationModal/TabSelector/index.jsx
+++ src/components/AuthenticationModal/TabSelector/index.jsx
@@ -3,8 +3,8 @@ export default function TabSelector({ children }) {
   return (
     <>
       <div className='nav nav-tabs nav-fill' role='tablist'>
-        <div className='nav-link rounded-0 active' id='nav-login-tab' data-bs-toggle='tab' data-bs-target='#nav-login' type='button' role='tab'>Login</div>
-        <div className='nav-link rounded-0' id='nav-registration-tab' data-bs-toggle='tab' data-bs-target='#nav-registration' type='button' role='tab'>Cadastro</div>
+        <div className='btn nav-item nav-link rounded-0 active' id='nav-login-tab' data-bs-toggle='tab' data-bs-target='#nav-login' role='tab'>Login</div>
+        <div className='btn nav-item nav-link rounded-0' id='nav-registration-tab' data-bs-toggle='tab' data-bs-target='#nav-registration' role='tab'>Cadastro</div>
       </div>
 
       <div className='tab-content'>
