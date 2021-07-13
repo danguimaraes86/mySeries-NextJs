491b00ff2b33e4c1ed8a99f20522d383c2766c28
Daniel Guimarães <danguimaraes86@gmail.com>
2021-07-13 16:39:13 -0300

    add: bootstrap class customization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
 
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
@@ -10,7 +11,7 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css' />
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css' />
         <link rel='icon' href='/favicon.ico' />
         <title>mySeries</title>
       </Head>
@@ -20,7 +21,7 @@ export default function MyApp({ Component, pageProps }) {
 
       <Component {...pageProps} />
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" />
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" />
     </div>
   )
 }
